6c1287101240d74ba8a03fe80c8ba855fb9ee23a
jby617 <qjadyd107@gmail.com>
2022-10-16 19:46:53 +0900

    1_commit

---
+++ src/Java_start.java
@@ -0,0 +1,7 @@
+public class Java_start {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("JAVA");
+        System.out.println("");
+    }
+}
